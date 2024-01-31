032aa361f985ef0a68f47bbd181b768fae123a7c
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-01-31 21:58:43 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+        
       </div>
       <h1>Vite + React</h1>
       <div className="card">
