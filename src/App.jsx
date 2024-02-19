6e2789a6a05166ce3a372f538f09aaa9942189be
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-02-19 21:49:08 +0530

    changed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import { useState } from 'react'
 
-import search_icon from '../src/Components/Assets/search.png';
-import clear_icon from '../src/Components/Assets/clear.png';
-import cloud_icon from '../src/Components/Assets/cloud.png';
 
 function App() {
   
--- src/Components/WeatherApp/weatherapp.jsx
+++ src/Components/WeatherApp/weatherapp.jsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import './weatherapp.css'
+
+import search_icon from '../Assets/search.png';
+import clear_icon from '../Assets/clear.png';
+import cloud_icon from '../Assets/cloud.png';
+import drizzle_icon from '../Assets/drizzle.png';
+import humidity_icon from '../Assets/humidity.png';
+
 const weatherapp = () => {
   return (
     <div>weatherapp</div>
