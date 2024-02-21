c10cadd8c0def48034a7c9b4de4a0a39761acd3a
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-02-21 20:43:22 +0530

    added images

--- src/Components/WeatherApp/weatherapp.jsx
+++ src/Components/WeatherApp/weatherapp.jsx
@@ -6,6 +6,9 @@ import clear_icon from '../Assets/clear.png';
 import cloud_icon from '../Assets/cloud.png';
 import drizzle_icon from '../Assets/drizzle.png';
 import humidity_icon from '../Assets/humidity.png';
+import rain_icon from '../Assets/rain.png';
+import snow_icon from '../Assets/snow.png';
+import wind_icon from '../Assets/wind.png';
 
 const weatherapp = () => {
   return (
