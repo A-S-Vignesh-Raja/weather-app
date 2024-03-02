2a2a527b19f9e7e318dcea8d47d9f29973e1a523
A-S-Vignesh-Raja <a.s.vigneshraja@gmail.com>
2024-03-02 11:49:56 +0530

    generated response in json format

--- src/Components/WeatherApp/weatherapp.jsx
+++ src/Components/WeatherApp/weatherapp.jsx
@@ -13,12 +13,14 @@ import wind_icon from '../Assets/wind.png';
 const Weatherapp = () => {
 
   let api_key="89331c2fdabeb440df05bc1574457ee8";
-  const search =()=>{
+  const search = async ()=>{
     const element=document.getElementsByClassName("cityInput");
     if(element[0].value===''){
       return 0;
     }
     let url=`https://api.openweathermap.org/data/2.5/weather?q=${element[0].value}&units=Metric&appid=${api_key}`;
+    let response= fetch(url);
+    let data=(await response).json();
   }
   return (
     <div className="container">
